15d81250c1bd9bae6b09f2b3f53a572a07d2a285
Prashant Maha rjan <rockeym50@gmail.com>
2022-10-26 13:37:55 +0545

    changing skills bar

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,4 @@
-import './skill.css'
+import "./skill.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Container from "react-bootstrap/Container";
@@ -8,116 +8,125 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Slide from "react-reveal/Slide";
 
+const SkillBar = ({ done }) => {
+  const [style, setStyle] = React.useState({});
 
+  setTimeout(() => {
+    const newStyle = {
+      opacity: 1,
+      width: `${done}%`,
+    };
 
+    setStyle(newStyle);
+  }, 200);
+
+  return (
+    <div className="skillbar">
+      <div className="skills" style={style}></div>
+    </div>
+  );
+};
 
 const Skills = () => {
   return (
-    
-        <Container id="skills">
-        <div className="title d-flex justify-content-center">
-          <span className="fs-1 heading">Skills</span>
-        </div>
-        <Container className='topics'>
-            <Row >
-                <Col>
-                <Slide left>
-                    <div className="mb-5">  
-                        <div className="topic d-flex justify-content-between">
-                            <span>HTML</span>
-                            <span>75%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay html"></div>
-                        </div>
-                    </div >
-                    <div className="mb-5">
-                        <div className="topic d-flex justify-content-between">
-                            <span>Javascript</span>
-                            <span>20%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay js"></div>
-                        </div>
-                    </div>
-                    <div className="mb-5">
-                        <div className="topic d-flex justify-content-between">
-                            <span>Photoshop</span>
-                            <span>60%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay ps"></div>
-                        </div>
-                    </div>
-                    <div className="mb-5">
-                        <div className="topic d-flex justify-content-between">
-                            <span>Indesign</span>
-                            <span>25%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay id"></div>
-                        </div>
-                    </div>
-                </Slide>
-                    
-                </Col>
-                <Col>
-                <Slide right>
-                 <div className="mb-5">
-                        <div className="topic d-flex justify-content-between">
-                            <span>CSS</span>
-                            <span>50%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay css"></div>
-                        </div>
-                    </div >
-                    <div className="mb-5">
-                        <div className="topic d-flex justify-content-between">
-                            <span>PHP</span>
-                            <span>20%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay php"></div>
-                        </div>
-                    </div>
-                    <div className="mb-5">
-                        <div className="topic d-flex justify-content-between">
-                            <span>Illustrator</span>
-                            <span>40%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay ill"></div>
-                        </div>
-                    </div>
-                    <div>
-                        <div className="topic d-flex justify-content-between">
-                            <span>Dart</span>
-                            <span>10%</span>
-                        </div>
-                        
-                        <div className='bg'>
-                            <div className="overlay dart"></div>
-                        </div>
-                        </div>
+    <Container id="skills">
+      <div className="title d-flex justify-content-center">
+        <span className="fs-1 heading">Skills</span>
+      </div>
+      <Container className="topics">
+        <Row>
+          <Col>
+            <Slide left>
+              <div className="mb-5">
+                <div className="topic d-flex justify-content-between">
+                  <span>HTML</span>
+                  <span>75%</span>
+                </div>
 
-                </Slide>
-                
-                </Col>
-            </Row>
-        </Container>
-        
+                <div className="bg">
+                  <SkillBar done="75" />
+                </div>
+              </div>
+              <div className="mb-5">
+                <div className="topic d-flex justify-content-between">
+                  <span>Javascript</span>
+                  <span>20%</span>
+                </div>
 
-        </Container>
-   
-  )
-}
+                <div className="bg">
+                  <SkillBar done="20" />
+                </div>
+              </div>
+              <div className="mb-5">
+                <div className="topic d-flex justify-content-between">
+                  <span>Photoshop</span>
+                  <span>60%</span>
+                </div>
 
-export default Skills
+                <div className="bg">
+                  <SkillBar done="60" />
+                </div>
+              </div>
+              <div className="mb-5">
+                <div className="topic d-flex justify-content-between">
+                  <span>Indesign</span>
+                  <span>25%</span>
+                </div>
+
+                <div className="bg">
+                  <SkillBar done="25" />
+                </div>
+              </div>
+            </Slide>
+          </Col>
+          <Col>
+            <Slide right>
+              <div className="mb-5">
+                <div className="topic d-flex justify-content-between">
+                  <span>CSS</span>
+                  <span>50%</span>
+                </div>
+
+                <div className="bg">
+                  <SkillBar done="50" />
+                </div>
+              </div>
+              <div className="mb-5">
+                <div className="topic d-flex justify-content-between">
+                  <span>PHP</span>
+                  <span>20%</span>
+                </div>
+
+                <div className="bg">
+                  <SkillBar done="20" />
+                </div>
+              </div>
+              <div className="mb-5">
+                <div className="topic d-flex justify-content-between">
+                  <span>Illustrator</span>
+                  <span>40%</span>
+                </div>
+
+                <div className="bg">
+                  <SkillBar done="40" />
+                </div>
+              </div>
+              <div>
+                <div className="topic d-flex justify-content-between">
+                  <span>Dart</span>
+                  <span>10%</span>
+                </div>
+
+                <div className="bg">
+                  <SkillBar done="10" />
+                </div>
+              </div>
+            </Slide>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+};
+
+export default Skills;
